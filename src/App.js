eefb902f20278cc2a80fb7667142725e8d7efce1
Sergey Gorshkov <gorshkovs372@gmail.com>
2021-11-07 14:39:41 +0300

    restart App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Header from "./Components/Header";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
       <div>
         <Header />
+        <Footer />
       </div>
   );
 }
-
 export default App;
